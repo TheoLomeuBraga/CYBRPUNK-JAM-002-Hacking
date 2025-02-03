2c8f19a091bb4e48f4ee46cf910692631d7a1f32
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-02-03 15:51:24 -0300

    a

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -8,20 +8,34 @@ class_name Enemy
 		return base_material
 	set(value):
 		base_material = value
-		for c1 in $template_psx_charter/Object/Skeleton3D.get_children():
-			for c2 in c1.get_children():
-				if c2 is MeshInstance3D:
-					c2.mesh.set("surface_0/material",value)
+		if $template_psx_charter/Object/Skeleton3D != null:
+			for c1 in $template_psx_charter/Object/Skeleton3D.get_children():
+				for c2 in c1.get_children():
+					if c2 is MeshInstance3D:
+						c2.mesh.set("surface_0/material",value)
 
 @export var outline_material : Material :
 	get():
 		return outline_material
 	set(value):
 		outline_material = value
-		for c1 in $template_psx_charter/Object/Skeleton3D.get_children():
-			for c2 in c1.get_children():
-				if c2 is MeshInstance3D:
-					c2.mesh.set("surface_1/material",value)
+		if $template_psx_charter/Object/Skeleton3D != null:
+			for c1 in $template_psx_charter/Object/Skeleton3D.get_children():
+				for c2 in c1.get_children():
+					if c2 is MeshInstance3D:
+						c2.mesh.set("surface_1/material",value)
+
+@export var walk_animation_speed : float : 
+	get():
+		if $template_psx_charter/AnimationTree != null:
+			return $template_psx_charter/AnimationTree.get("parameters/walk/blend_position")
+		return 0
+	set(value):
+		if $template_psx_charter/AnimationTree != null:
+			$template_psx_charter/AnimationTree.set("parameters/walk/blend_position",value)
+
+func shot() -> void:
+	$template_psx_charter/AnimationTree.set("parameters/OneShot/request","fire")
 
 func _ready() -> void:
 	if not Engine.is_editor_hint():
